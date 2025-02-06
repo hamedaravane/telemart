7bf767e71620661efcc012d6758cc5f7f1e0496e
Hamed Arghavan <hamedaravane@gmail.com>
2025-02-06 13:13:21 +0330

    feat(users): implement basic user CRUD operations
    - Add UsersModule, UsersController, and UsersService
    - Create User entity and set up TypeORM integration
    - Implement methods to get all users, get user by Telegram ID, and create a new user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  async getAllUsers(): Promise<User[]> {
+    return this.usersService.getAllUsers();
+  }
+
+  @Get(':telegramId')
+  async getUserByTelegramId(
+    @Param('telegramId') telegramId: string,
+  ): Promise<User | null> {
+    return this.usersService.findByTelegramId(telegramId);
+  }
+
+  @Post()
+  async createUser(
+    @Body('telegramId') telegramId: string,
+    @Body('name') name: string,
+    @Body('phoneNumber') phoneNumber?: string,
+    @Body('email') email?: string,
+  ): Promise<User> {
+    return this.usersService.createUser(telegramId, name, phoneNumber, email);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async findByTelegramId(telegramId: string): Promise<User | null> {
+    return this.usersRepository.findOne({ where: { telegramId } });
+  }
+
+  async createUser(
+    telegramId: string,
+    name: string,
+    phoneNumber?: string,
+    email?: string,
+  ): Promise<User> {
+    const user = this.usersRepository.create({
+      telegramId,
+      name,
+      phoneNumber,
+      email,
+    });
+    return this.usersRepository.save(user);
+  }
+
+  async getAllUsers(): Promise<User[]> {
+    return this.usersRepository.find();
+  }
+}
