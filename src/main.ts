6e5063b9d63371fb7f893d948708b4896769b3b3
Hamed Arghavan <hamedaravane@gmail.com>
2025-02-14 00:41:55 +0330

    feat(config): replace dotenv with nestjs config module
    - Remove dotenv package and its usage
    - Introduce @nestjs/config module for environment variable management
    - Update server initialization to use ConfigService
    - Add port configuration and log server startup message

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { config } from 'dotenv';
+import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  config();
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<number>('PORT') || 3000;
+  await app.listen(port);
+  console.log(`Server running on port ${port}`);
 }
 bootstrap();
