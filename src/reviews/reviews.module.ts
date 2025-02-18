70391d7e89492f862d4f3260936674c3548ef3de
Hamed Arghavan <hamedaravane@gmail.com>
2025-02-18 23:13:53 +0330

    feat(reviews): add review reply and report entities
    - Import ReviewReply and ReviewReport entities in reviews.module.ts
    - Update TypeOrmModule to include new entities

--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -3,11 +3,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReviewsService } from './reviews.service';
 import { ReviewsController } from './reviews.controller';
 import { Review } from './review.entity';
+import { ReviewReply } from './review-reply.entity';
+import { ReviewReport } from './review-report.entity';
 import { UsersModule } from '../users/users.module';
 import { ProductsModule } from '../products/products.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Review]), UsersModule, ProductsModule],
+  imports: [
+    TypeOrmModule.forFeature([Review, ReviewReply, ReviewReport]),
+    UsersModule,
+    ProductsModule,
+  ],
   providers: [ReviewsService],
   controllers: [ReviewsController],
   exports: [ReviewsService],
