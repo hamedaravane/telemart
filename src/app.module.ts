a46c2bc41c87aa7b4cc4aca073217854152d6d35
Hamed Arghavan <hamedaravane@gmail.com>
2025-03-03 14:01:19 +0330

    feat(infrastructure): update Redis configuration and remove Docker Compose
    - Add Redis configuration variables to .env.example
    - Remove docker-compose.yml file
    - Update Redis connection in app.module.ts to use new environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { ReviewsModule } from './reviews/reviews.module';
 import { TelegramUserService } from './telegram/telegram-user.service';
 import { HttpModule } from '@nestjs/axios';
 import { ScheduleModule } from '@nestjs/schedule';
-import { BullModule, BullRootModuleOptions } from '@nestjs/bull';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
   imports: [
@@ -27,10 +27,11 @@ import { BullModule, BullRootModuleOptions } from '@nestjs/bull';
     }),
     BullModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService): BullRootModuleOptions => ({
+      useFactory: (configService: ConfigService) => ({
         redis: {
-          host: configService.get<string>('REDIS_HOST', 'localhost'),
-          port: +configService.get<number>('REDIS_PORT', 6379),
+          host: configService.get<string>('REDIS_HOST_PRIVATE'),
+          port: +configService.get<number>('REDIS_PORT_PRIVATE', 6379),
+          password: configService.get<string>('REDIS_PASSWORD'),
         },
       }),
       inject: [ConfigService],
