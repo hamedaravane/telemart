cd34b1b9c6f24996958fa47fc184e7f23a61c729
Hamed Arghavan <hamedaravane@gmail.com>
2025-03-03 13:15:10 +0330

    refactor(app.module): ensure REDIS_PORT is parsed as number
     Redis port number should be parsed as a number to prevent unexpected behavior when connecting to the Redis server. The ConfigService now explicitly converts the 'REDIS_PORT' configuration to a number using the unary plus operator.

--- src/app.module.ts
+++ src/app.module.ts
@@ -31,7 +31,7 @@ import { BullModule } from '@nestjs/bull';
       useFactory: (configService: ConfigService) => ({
         redis: {
           host: configService.get<string>('REDIS_HOST', 'localhost'),
-          port: configService.get<number>('REDIS_PORT', 6379),
+          port: +configService.get<number>('REDIS_PORT', 6379),
         },
       }),
     }),
