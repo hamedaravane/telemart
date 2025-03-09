b532c8d46af2a75c16b638f5db2fe657673d3ee5
Hamed Arghavan <hamedaravane@gmail.com>
2025-03-09 13:08:46 +0330

    refactor(market): improve market data fetching and module setup
    - Set up MarketModule with required services and controllers- Refactor MarketService to use async data fetching
    - Optimize product and store filtering and sorting logic
    - Improve code readability and maintainability

--- src/market/market.controller.ts
+++ src/market/market.controller.ts
@@ -1,6 +1,7 @@
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Controller, Get } from '@nestjs/common';
 import { MarketService } from './market.service';
+import { MarketPageResponse } from './market.entity';
 
 @ApiTags('Market')
 @Controller('market')
@@ -11,6 +12,7 @@ export class MarketController {
   @ApiResponse({
     status: 200,
     description: 'The market page data',
+    type: MarketPageResponse,
   })
   async getMarketData() {
     return await this.marketService.getMarketData();
