3873a4e73c9767b137937e94aa361bd3bbe4d2de
Hamed <hamedaravane@gmail.com>
2025-04-03 15:13:04 +0330

    refactor(users): update user mapping functions and DTOs
    - Rename interfaces to DTOs for better naming convention
    - Update property names and structures to match new DTOs
    - Add nullish coalescing and conditional assignments for improved data handling
    - Refactor photo and logo properties to have consistent structures
    - Update deliveryDate to use default value when undefined

--- src/users/mappers/user.mapper.ts
+++ src/users/mappers/user.mapper.ts
@@ -1,27 +1,30 @@
 import { User } from '../user.entity';
-import { UserPrivateProfile, UserPublicPreview, UserSummary } from './types';
+import {
+  UserPrivateProfileDto,
+  UserPublicPreviewDto,
+  UserSummaryDto,
+} from '@/users/dto';
 
-export function mapUserToPublicPreview(user: User): UserPublicPreview {
+export function mapUserToPublicPreview(user: User): UserPublicPreviewDto {
   return {
     id: user.id,
-    username: user.name,
+    username: user.username,
     handle: undefined,
-    photo: undefined,
+    photo: user.photoUrl ? { url: user.photoUrl } : undefined,
   };
 }
 
-export function mapUserToSummary(user: User): UserSummary {
-  const [firstName, ...rest] = user.name.split(' ');
+export function mapUserToSummary(user: User): UserSummaryDto {
   return {
     ...mapUserToPublicPreview(user),
-    firstName,
-    lastName: rest.join(' ') || undefined,
+    firstName: user.firstName,
+    lastName: user.lastName,
     role: user.role,
-    address: null,
+    address: undefined, // TODO: should be changes later
   };
 }
 
-export function mapUserToPrivateProfile(user: User): UserPrivateProfile {
+export function mapUserToPrivateProfile(user: User): UserPrivateProfileDto {
   return {
     ...mapUserToSummary(user),
     telegramId: user.telegramId,
@@ -31,7 +34,7 @@ export function mapUserToPrivateProfile(user: User): UserPrivateProfile {
     stores: user.stores?.map((store) => ({
       id: store.id,
       name: store.name,
-      logo: store.logoUrl ? [{ url: store.logoUrl }] : [],
+      logo: store.logoUrl ? { url: store.logoUrl } : undefined,
       reputation: store.reputation,
       isActive: true,
     })),
@@ -42,11 +45,11 @@ export function mapUserToPrivateProfile(user: User): UserPrivateProfile {
       store: {
         id: order.store?.id,
         name: order.store?.name,
-        logo: order.store?.logoUrl ? [{ url: order.store.logoUrl }] : [],
+        logo: order.store?.logoUrl ? { url: order.store.logoUrl } : undefined,
         reputation: order.store?.reputation || 5,
         isActive: true,
       },
-      deliveryDate: order.deliveryDate,
+      deliveryDate: order.deliveryDate ?? new Date(), // TODO: should be changes later
       createdAt: order.createdAt,
     })),
   };
